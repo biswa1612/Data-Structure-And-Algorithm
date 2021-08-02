d9fba323a3f97e3c6951f61fba1abd66f56caa05
Biswajit Rout <b119018@iiit-bh.ac.in>
2021-08-02 22:32:27 +0530

    bit manipulation

---
+++ Sieve of Eratosthenes/prime_factorisation.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+void factorisation(int n){
+    int pf[n+1] = {0};
+    for(int i=2;i<=n;i++){
+        pf[i]=i;
+    }
+    for(int i=2;i<=n;i++){
+        if(pf[i]==i){
+            for(int j=i*i;j<=n;j+=i){
+                if(pf[j]==j){
+                    pf[j]=i;
+                }
+            }
+        }
+    }
+    while(n!=1){
+        cout<<pf[n]<<" ";
+        n=n/pf[n];
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    factorisation(n);
+}
